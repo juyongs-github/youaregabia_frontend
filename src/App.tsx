a6c986cc46fc57716d7898e1bdc1581ce40e5ccc
yong <jylee0097@gmail.com>
2026-01-13 21:04:51 +0900

    feat: 프론트엔드 프로젝트 초기 세팅

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="bg-sky-300">
+      <h1 className="text-xl font-bold">tailwindCSS 적용 테스트</h1>
+    </div>
+  );
+}
+
+export default App;
