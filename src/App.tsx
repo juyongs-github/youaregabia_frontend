fdd5f0349a44c290f22a4ecf24a47598bb96d3e8
KimGyeongmin <kkm01054354772@gmail.com>
2026-01-22 15:09:39 +0900

    01/22 레이아웃 병합 후

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { Routes, Route } from "react-router-dom";
 import "./App.css";
 import Layout from "./layout/Layout";
+import HomePage from "./pages/HomePage";
 // import SearchResult from "./pages/SearchResult";
 // import Home from "./pages/Home";
 
@@ -9,8 +10,7 @@ function App() {
     <Routes>
       {/* Layout 적용 화면만 Layout Route 안에 Route 추가 */}
       <Route element={<Layout />}>
-        {/* <Route path="/" element={<Home />} />
-        <Route path="/search" element={<SearchResult />} /> */}
+        <Route path="/" element={<HomePage />} />
       </Route>
       {/* 아닌 것들은 여기 밑으로 Route 추가 */}
     </Routes>
